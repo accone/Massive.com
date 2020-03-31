dad4b401bb539023344ac81c0ed7ac6ff7ffa4a0
accone <avira222217@gmail.com>
2020-03-31 18:16:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,13 @@ document.getElementById('add').onclick = function(){
 
     if (name.length != 0 && surname.length != 0 && email.length != 0 && password.length != 0){
         alert("An error has occurred! Most likely you did not enter all the data!");
-        $('#submit').attr('disabled', 'disabled');       
+        $('#submit').attr();       
     }
 
     register_caches.push(cache)
     saveRegister_cache();
 }
 
-function saveRegister_cache(){
-    if (name.length != 1 && surname.length != 1 && email.length != 1 && password.length != 1){    
-        localStorage.setItem('register_caches', JSON.stringify(register_caches));
-    }
+function saveRegister_cache(){   
+    localStorage.setItem('register_caches', JSON.stringify(register_caches));
 }
